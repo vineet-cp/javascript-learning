107372bfe3a4e855ea89434772769d802756a9fe
vineet21tiwari <vineetjeccs@gmail.com>
2026-08-26 06:45:56 +0530

    Add module 1 javascript practice

---
+++ module-1-javascript/first-script.js
@@ -0,0 +1,9 @@
+// console is an object provided by the environment
+// . accesses something inside the object and log is a function used to print object
+// () contains the information passed to the function
+
+// console.log("hello , students!")
+// console.log(10);
+console.log("10")
+console.log(10+20)
+console.log("10+20")
