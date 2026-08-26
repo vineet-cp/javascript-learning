a6bf156a591b0b4fc00dfca026ba9a264ef346be
asirwadh <john.panuganti@gmail.com>
2026-08-25 21:44:03 -0400

    This is the Test report

---
+++ module-1-javascript/Assignment1/q5-login-story.js
@@ -0,0 +1,30 @@
+// Create q5-login-story.js. Present a clean, readable test-case report using only console.log().
+// Your program must include:
+// 01 A title: VALID LOGIN TEST
+// 02 Application name and browser name
+// 03 Five numbered test steps, each printed on a separate line
+// 04 Expected result and actual result
+// 05 A final status line: TEST PASSED
+// 06 One single-line comment and one multi-line comment in the source code
+// Design challenge: use symbols such as =, - or * to make the report easy to read. Do not copy the
+// output design from Questions 1 or 2 exactly.
+console.log ()
+console.log ("=".repeat(10) + " VALID LOGIN TEST  " + "=".repeat(10))
+console.log ()
+console.log ("Online Email " + " - " + "Browser - Chrome " ) // (navigator.userAgentData.navigator.userAgentData));
+console.log ()
+console.log("TEST 1 -  Go to Mail.yahoo.com ")
+console.log("Test 2 - Input Username and press next button")
+console.log("Test 3 - Inupt password and press next button")
+console.log("Test 4 - Press Next button for crediential validated" )
+console.log("Test 5 - From the Email Dash board, click on the email and the email open up")
+console.log ()
+console.log("Steps\t      Expected Results         \t                     Actual Results");
+console.log("Test1\t Browser loads yahoo mail page \t                 Browser loaded Yahoo mail page");
+console.log("Test2\t Input Valid User name and press next \t         Allowed Input valid user name and pressed next");
+console.log("Test3\t Input Password and press next \t                 Allowed password and Press next");
+console.log("Test4\t Browser will display the  email dash board \t Email Dashboard is displayed");
+console.log("Test5\t Click on the email and the content is loaded \t Email content is displayed");
+console.log ("=".repeat(90))
+console.log("TEST PASSED ")
+console.log ("=".repeat(90))
