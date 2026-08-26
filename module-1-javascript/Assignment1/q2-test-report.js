9719e6eb5bcb404768d2d3c1107b514af9b900bc
asirwadh <john.panuganti@gmail.com>
2026-08-25 21:35:30 -0400

    My First assignment commit

---
+++ module-1-javascript/Assignment1/q2-test-report.js
@@ -0,0 +1,11 @@
+
+// Exercise 2 08/24/2026
+console.log("-".repeat(5) + " TEST EXECUTION REPORT " + "-".repeat(5));
+console.log("Application: Demo Shopping App")
+console.log("Passed: 18")
+console.log("Failed: 5")
+console.log("Skipped: 2")
+let total = ((18 + 5 + 2))
+console.log("Total : " + total)
+//console.log("Total : " + (18 + 5 + 2))
+console.log("-".repeat(33)) 
