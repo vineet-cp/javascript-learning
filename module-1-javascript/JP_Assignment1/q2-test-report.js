1b1166898643cfa7c7b4e349a65a92351ef5640e
asirwadh <john.panuganti@gmail.com>
2026-08-26 22:53:34 -0400

    The changes are due to Variable declaration

--- module-1-javascript/JP_Assignment1/q2-test-report.js
+++ module-1-javascript/JP_Assignment1/q2-test-report.js
@@ -6,6 +6,17 @@ console.log("Passed: 18")
 console.log("Failed: 5")
 console.log("Skipped: 2")
 let total = ((18 + 5 + 2))
+let age =35
+console.log(age)
+age = 40
+console.log(age)
+const myURL ="http://example.com"
+console.log(myURL)
+//myURL ="https://amazon.com"
+var ageVar =32
+console.log(ageVar)
+ageVar = 42
+console.log(ageVar)
 console.log("Total : " + total)
 //console.log("Total : " + (18 + 5 + 2))
 console.log("-".repeat(33)) 
