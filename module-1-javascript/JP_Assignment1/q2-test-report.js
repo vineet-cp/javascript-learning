b9a43171304a8590c3e0c4ed8306c859a94fedae
asirwadh <john.panuganti@gmail.com>
2026-08-27 21:00:55 -0400

    Added Let , Var statements

--- module-1-javascript/JP_Assignment1/q2-test-report.js
+++ module-1-javascript/JP_Assignment1/q2-test-report.js
@@ -1,4 +1,4 @@
-
+//node .\module-1-javascript\JP_Assignment1\q2-test-report.js
 // Exercise 2 08/24/2026
 console.log("-".repeat(5) + " TEST EXECUTION REPORT " + "-".repeat(5));
 console.log("Application: Demo Shopping App")
@@ -7,16 +7,16 @@ console.log("Failed: 5")
 console.log("Skipped: 2")
 let total = ((18 + 5 + 2))
 let age =35
-console.log(age)
+console.log( "Age during set " + age)
 age = 40
-console.log(age)
+console.log( "Age After set " + age)
 const myURL ="http://example.com"
 console.log(myURL)
 //myURL ="https://amazon.com"
 var ageVar =32
-console.log(ageVar)
+console.log(  "Age during Var " + ageVar)
 ageVar = 42
-console.log(ageVar)
+console.log("Age After Var " + ageVar)
 console.log("Total : " + total)
 //console.log("Total : " + (18 + 5 + 2))
 console.log("-".repeat(33)) 
