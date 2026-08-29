66ad1076d90c7facb705c2d3c5de544bb80394f1
Bhawna Jha <bhawna.amarnath@gmail.com>
2026-08-28 22:07:24 -0400

    initial push

---
+++ module-1-javascript/Bhawna's_assignment1/Welcome card/q5-login-story.js
@@ -0,0 +1,10 @@
+console.log("========VALID LOGIN TEST==========");
+console.log('https://www.saucedemo.com, chrome');
+console.log("Enter Username: ");
+console.log("Enter password:");
+console.log("Click on Login button")
+console.log("Select an item and click on add to cart")
+console.log("Complete the checkout process");
+console.log("User should be able to login, select an item and process their payment successful")
+console.log("==========Test Passed==========");
+
---
+++ module-1-javascript/Bhawna's_assignment1/Welcome card/q7-simplemathwithvariables.js
@@ -0,0 +1,11 @@
+let a=20;
+let b= 40;
+console.log(a+b);
+let c = a * b;
+console.log(c);
+let d = b / a;
+console.log(d);
+let e = a % b;
+console.log(e);
+
+console.log("**********End of Simple Math with Variables***********");
